9a2d7ce588eab5aa364017ed49c841ebf0dd2807
Jonas Minnberg <sasq64@gmail.com>
2019-07-05 12:01:54 +0200

    Try to handle not having libfmt present

--- src/coreutils/format.h
+++ src/coreutils/format.h
@@ -1,27 +1,52 @@
 #pragma once
 
 #include <cstring>
+#include <string>
 #include <vector>
 
-#include <fmt/format.h>
-#include <fmt/printf.h>
+#ifdef USE_FMT
+#    include <fmt/format.h>
+#    include <fmt/printf.h>
 
 namespace utils {
 
-template <typename T> struct FixPtr { using type = T; };
-
-template <typename T> struct FixPtr<T *> { using type = void *; };
-
-template <class... A> std::string format(const std::string &fmt, A &&... args) {
-	return fmt::sprintf(fmt, std::forward<A>(args)...);
+template <class... A> std::string format(const std::string& fmt, A&&... args)
+{
+    return fmt::sprintf(fmt, std::forward<A>(args)...);
 }
 
-template <class... A> void print_fmt(const std::string &fmt, A &&... args) {
-	fmt::printf(fmt, std::forward<A>(args)...);
+template <class... A> void print_fmt(const std::string& fmt, A&&... args)
+{
+    fmt::printf(fmt, std::forward<A>(args)...);
 }
 
-template <class... A> void println(const std::string &fmt = "", A &&... args) {
-	fmt::printf(fmt + "\n", std::forward<A>(args)...);
+template <class... A> void println(const std::string& fmt = "", A&&... args)
+{
+    fmt::printf(fmt + "\n", std::forward<A>(args)...);
 }
 
 } // namespace utils
+
+#else
+
+namespace utils {
+
+template <class... A> std::string format(const std::string& fmt, A&&... args)
+{
+    return fmt; // fmt::sprintf(fmt, std::forward<A>(args)...);
+}
+
+template <class... A> void print_fmt(const std::string& fmt, A&&... args)
+{
+    // fmt::printf(fmt, std::forward<A>(args)...);
+}
+
+template <class... A> void println(const std::string& fmt = "", A&&... args)
+{
+    // fmt::printf(fmt + "\n", std::forward<A>(args)...);
+}
+
+} // namespace utils
+
+#endif
+
